e6906e4caf75da67aebdbffee0b385995955b23e
Ned Batchelder <ned@nedbatchelder.com>
2017-09-10 17:32:13 -0400

    Where did these semicolons come from?

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from 'react'
+import ReactDOM from 'react-dom'
 import { createStore } from 'redux'
 import { Provider } from 'react-redux'
 
-import './index.css';
-import App from './typing/app.js';
+import './index.css'
+import App from './typing/app'
 import typing_reducer from './typing/reducers'
-import registerServiceWorker from './registerServiceWorker';
+import registerServiceWorker from './registerServiceWorker'
 
 
 const store = createStore(typing_reducer)
