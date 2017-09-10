30a9a42fe43455a148860928afdbef4cac333863
Ned Batchelder <ned@nedbatchelder.com>
2017-09-10 13:10:08 -0400

    Calendar headings

--- src/Typing.js
+++ src/Typing.js
@@ -33,6 +33,8 @@ const ShoppingList = () => (
 class Calendar extends React.Component {
   render() {
     var cal = [];
+    let days = ["Sun", "Mon", "Tues", "Weds", "Thur", "Fri", "Sat"].map(day => <th>{day}</th>);
+    cal.push(<tr>{days}</tr>);
     for (let row = 0; row < 4; row++) {
       let days = [];
       for (let day = 0; day < 7; day++) {
