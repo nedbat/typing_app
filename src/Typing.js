d7505e63040a123bbf236cb0a6f950bbac271dfd
Ned Batchelder <ned@nedbatchelder.com>
2017-09-10 11:55:26 -0400

    Multi-screen

--- src/Typing.js
+++ src/Typing.js
@@ -6,15 +6,37 @@ import {
 } from 'react-router-dom'
 
 
+const Home = () => (
+  <div>
+    <p>I want to:</p>
+    <ul>
+      <li><Link to="/shopping">type a shopping list</Link></li>
+      <li><Link to="/calendar">type on my calendar</Link></li>
+    </ul>
+  </div>
+)
+
+const Done = () => (
+  <div>
+    <Link to="/">I'm done.</Link>
+  </div>
+)
+
+const ShoppingList = () => (
+  <div>
+    <h1>Shopping list</h1>
+    <p>(more here)</p>
+    <Done/>
+  </div>
+)
+
 const TypingApp = () => (
   <Router>
     <div>
-      <p>I want to:</p>
-      <ul>
-        <li><Link to="/shopping">type a shopping list</Link></li>
-        <li><Link to="/calendar">type on my calendar</Link></li>
-      </ul>
+      <Route exact path="/" component={Home}/>
+      <Route path="/shopping" component={ShoppingList}/>
     </div>
   </Router>
 )
+
 export default TypingApp
