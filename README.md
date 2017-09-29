3b0ff22478e9a2333bec75abea170b743b75e3c0
Ned Batchelder <ned@nedbatchelder.com>
2017-09-29 17:10:30 -0400

    Inching toward persistence

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 A simple application to learn React etc.
+
+
+Firebase data:
+
+{
+  'user1': {
+    'shop': ['a', 'b', 'c', 'd'],
+    'cal': {
+      '20170929': ['a', ...],
+      '20170930': ['a', ...],
+    },
+  ...
+}
--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import './index.css'
 import App from './typing/app'
 import typing_reducer from './typing/reducers'
 import registerServiceWorker from './registerServiceWorker'
-import firebaseDb from './firebase'
+import { firebaseDb } from './firebase'
 
 const store = createStore(typing_reducer)
 
